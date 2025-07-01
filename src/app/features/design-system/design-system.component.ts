e569af0578b9d3f16610d12b1d5bb410ccf0561f
10GO <mixelidzet@gmail.com>
2025-07-01 11:31:05 +0400

    feat - folder structure

---
+++ src/app/features/design-system/design-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-system',
+  imports: [],
+  templateUrl: './design-system.component.html',
+  styleUrl: './design-system.component.scss'
+})
+export class DesignSystemComponent {
+
+}
